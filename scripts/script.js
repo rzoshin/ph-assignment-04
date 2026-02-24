1c85023a9505a67da2a781f796884e8126b79076
rzoshin <raiyan435jgcc@gmail.com>
2026-02-24 14:41:39 +0600

    Delete functionality from all tabs done

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 const total = document.getElementById("totalCount");
 const interviewCount = document.getElementById("interviewCount");
 const rejectedCount = document.getElementById("rejectedCount");
+const headCount = document.getElementById("No-of-jobs");
 
 let interviewArray = [];
 let rejectedArray = [];
@@ -32,8 +33,6 @@ function showOnly(id) {
     const rejectedTab = document.getElementById("rejected-tab");
     const allTab = document.getElementById("all-tab");
 
-    const headCount = document.getElementById("No-of-jobs");
-
     interviewTab.classList.remove("bg-[#3B82F6]", "text-white");
     rejectedTab.classList.remove("bg-[#3B82F6]", "text-white");
     allTab.classList.remove("bg-[#3B82F6]", "text-white");
@@ -49,7 +48,7 @@ function showOnly(id) {
     else if (id == "jobs-card") {
         allTab.classList.add("bg-[#3B82F6]", "text-white");
         headCount.innerText = total.innerText;
-    }     
+    }
 }
 
 
@@ -65,7 +64,7 @@ mainContainer.addEventListener("click", function (event) {
         const description = parent.querySelector(".description").innerText;
         const status = parent.querySelector(".state").innerText;
         const details = parent.querySelector(".details").innerText;
-
+        console.log(event.target);
         const cardInfo = {
             jobName,
             bio,
@@ -81,6 +80,7 @@ mainContainer.addEventListener("click", function (event) {
         }
         if (rejectedExist) {
             rejectedArray = rejectedArray.filter(job => job.jobName != cardInfo.jobName);
+            headCount.innerText = rejectedArray.length;
             renderRejected();
         }
         calculateCount();
@@ -111,20 +111,37 @@ mainContainer.addEventListener("click", function (event) {
 
         if (interviewExist) {
             interviewArray = interviewArray.filter(job => job.jobName != cardInfo.jobName);
+            headCount.innerText = interviewArray.length;
             renderInterview();
         }
         calculateCount();
         renderRejected();
     }
-    else if (event.target.classList.contains("trash") ) {
+    else if (event.target.classList.contains("trash")) {
         const parent = event.target.parentNode.parentNode;
+        const grandParent = parent.parentNode;
+        console.log(grandParent.id);
         parent.remove();
-        calculateCount();
+        headCount.innerText = grandParent.children.length;
+        if (grandParent.id == "interview-section") {
+            interviewCount.innerText = grandParent.children.length;
+        }
+        if (grandParent.id == "rejected-section") {
+            rejectedCount.innerText = grandParent.children.length;
+        }  
     }
-    else if (event.target.classList.contains("delete")){
+    else if (event.target.classList.contains("delete")) {
         const parent = event.target.parentNode;
+        const grandParent = parent.parentNode;
+
         parent.remove();
-        calculateCount();
+        headCount.innerText = grandParent.children.length;
+        if (grandParent.id == "interview-section") {
+            interviewCount.innerText = grandParent.children.length;
+        }
+        if (grandParent.id == "rejected-section") {
+            rejectedCount.innerText = grandParent.children.length;
+        }
     }
 
 });
@@ -138,35 +155,48 @@ function renderInterview() {
     for (let job of interviewArray) {
         let div = document.createElement("div");
         div.className = "p-6 bg-base-100 border border-[#F1F2F4] flex justify-between";
-        div.innerHTML = `<div> <h4 class="jobName mb-1 text-lg font-semibold"> ${job.jobName}</h4><p class="  bio text-base text-[#64748B] font-regular"> ${job.bio}</p> <p class="description my-5 font-regular text-sm text-[#64748B]"> ${job.description}</p> <div class="state mb-2 py-2 px-3 text-sm font-medium bg-[#e2fee4] w-fit"> ${job.status} </div><p class="details mb-5 text-[#323B49] text-sm"> ${job.details} </p><div class="btn-container space-x-2"><button class="interview btn btn-success btn-outline">INTERVIEW</button><button class="rejected btn btn-error btn-outline">REJECTED</button></div></div> <button class="delete btn btn-circle text-[#64748B]"> <i id="trash" class="fa-regular fa-trash-can"></i> </button>`
+        div.innerHTML = `<div> <h4 class="jobName mb-1 text-lg font-semibold"> ${job.jobName}</h4><p class="  bio text-base text-[#64748B] font-regular"> ${job.bio}</p> <p class="description my-5 font-regular text-sm text-[#64748B]"> ${job.description}</p> <div class="state mb-2 py-2 px-3 text-sm font-medium bg-[#e2fee4] w-fit"> ${job.status} </div><p class="details mb-5 text-[#323B49] text-sm"> ${job.details} </p><div class="btn-container space-x-2"><button class="interview btn btn-success btn-outline">INTERVIEW</button><button class="rejected btn btn-error btn-outline">REJECTED</button></div></div> <button class="delete btn btn-circle text-[#64748B]"> <i class="trash fa-regular fa-trash-can"></i> </button>`
         interviewSection.appendChild(div);
     }
-    if(interviewArray.length == 0){
+    if (interviewArray.length == 0) {
         let div = document.createElement("div");
         div.className = "py-[60px] px-10 bg-base-100 text-center";
         div.innerHTML = `<img class="mx-auto mb-5"src="./nothing.png" alt="">
                         <h2 class="text-2xl font-semibold mb-1">No jobs available</h2>
                         <p class="text-base">Check back soon for new job opportunities</p>`
-    
+
         interviewSection.appendChild(div);
     }
-    
+
 }
 
 function renderRejected() {
     const rejectedSection = document.getElementById("rejected-section");
     rejectedSection.classList.add("space-y-4", "mb-5");
     rejectedSection.innerHTML = " ";
-    
+
     for (let job of rejectedArray) {
-        
+
         let div = document.createElement("div");
-        
+
         div.className = "p-6 bg-base-100 border border-[#F1F2F4] flex justify-between";
-        div.innerHTML = `<div> <h4 class="jobName mb-1 text-lg font-semibold"> ${job.jobName}</h4><p class="  bio text-base text-[#64748B] font-regular"> ${job.bio}</p> <p class="description my-5 font-regular text-sm text-[#64748B]"> ${job.description}</p> <div class="state mb-2 py-2 px-3 text-sm font-medium bg-[#FEE4E2] w-fit"> ${job.status} </div><p class="details mb-5 text-[#323B49] text-sm"> ${job.details} </p><div class="btn-container space-x-2"><button class="interview btn btn-success btn-outline">INTERVIEW</button><button class="rejected btn btn-error btn-outline">REJECTED</button></div> </div> <button class="delete btn btn-circle text-[#64748B]"> <i id="trash" class="fa-regular fa-trash-can"></i> </button>`
+        div.innerHTML = `<div> 
+            <h4 class="jobName mb-1 text-lg font-semibold"> ${job.jobName}</h4>
+            <p class="bio text-base text-[#64748B] font-regular"> ${job.bio}</p> 
+            <p class="description my-5 font-regular text-sm text-[#64748B]"> ${job.description}</p> 
+            <div class="state mb-2 py-2 px-3 text-sm font-medium bg-[#FEE4E2] w-fit"> ${job.status} </div>
+            <p class="details mb-5 text-[#323B49] text-sm"> ${job.details} </p>
+            <div class="btn-container space-x-2">
+                <button class="interview btn btn-success btn-outline">INTERVIEW</button>
+                <button class="rejected btn btn-error btn-outline">REJECTED</button>
+            </div> 
+        </div> 
+        <button class="delete btn btn-circle text-[#64748B]"> 
+            <i class="trash fa-regular fa-trash-can"></i> 
+        </button>`
         rejectedSection.appendChild(div);
     }
-    if(rejectedArray.length == 0){
+    if (rejectedArray.length == 0) {
         let div = document.createElement("div");
         div.className = "py-[60px] px-10 bg-base-100 text-center";
         div.innerHTML = `<img class="mx-auto mb-5"src="./nothing.png" alt="">
@@ -174,7 +204,7 @@ function renderRejected() {
                         <p class="text-base">Check back soon for new job opportunities</p>`
         rejectedSection.appendChild(div);
     }
-    
+
 }
 
 // Delete Functionality
